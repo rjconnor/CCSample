18f446fd9ea66cada7c6ac242095166f1e34766b
Nigel Wardle <n.wardle@uk.cdw.com>
2019-11-14 16:54:09 +0000

    added aminal test class

---
+++ app/Domain/Tiger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Domain
+{
+    public class Tiger
+    {
+        public string Name { get; set; }
+
+        public string MakeNoise()
+        {
+            return "Meow";
+        }
+    }
+}
---
+++ test/ZooTest.cs
@@ -0,0 +1,19 @@
+using app.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+    [TestClass]
+    public class ZooTest
+    {
+        [TestMethod]
+        public void TestAnimalName()
+        {
+            var name = "tiddles";
+
+            var cat = new Tiger { Name = name };
+
+            Assert.IsTrue(string.Compare(name, cat.Name, true) == 0);
+        }
+    }
+}
