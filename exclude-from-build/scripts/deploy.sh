ba80e8616d2b31b0b1e692acf3e65812d9831fb2
Nigel Wardle <n.wardle@uk.cdw.com>
2019-11-24 17:11:31 +0000

    updated deploy.sh

--- exclude-from-build/scripts/deploy.sh
+++ exclude-from-build/scripts/deploy.sh
@@ -13,22 +13,18 @@ https://www.danielstechblog.io/build-azure-kubernetes-service-cluster-with-bring
 
 COMMENT
 
-# ----------- Define Variables --------------
 SUBSCRIPTIONNAME="Clyde & Co - UK - Leap - Sandbox - CDW UK CSP"
 SUBSCRIPTIONID="00bcabe9-0608-46f3-a624-a53c00ecbf5b"
 
-# Set subscription
 az account set --subscription $SUBSCRIPTIONID
 
-ORG='cc'
-ENV='sb' 
-TAGS="environment=$ENV id=- name=- Application-Taxonomy=- Billed-To=- IT-Owner-Contact=- Business-Owner-Contact=- Days-Operational=0 Hours-Operational=0 Service=-"
-echo $TAGS
-
+ORG="cc"
+ENV="sb" 
+EnvFull="Sandbox"
+TAGS="Environment=$EnvFull Application-Taxonomy=- Billed-Internally-To=- IT-Owner-Contact=- Business-Owner-Contact=- Days-Operational=0 Hours-Operational=0 Service=-"
 LOCATION="uksouth"
 REGIONSHORT='uks'
 
-# Resource groups
 GEN_RG="${ORG}-${REGIONSHORT}-rsg-leap-${ENV}"
 HUB_RG="${ORG}-${REGIONSHORT}-rsg-leap-${ENV}-hub"
 AKS_RG="${ORG}-${REGIONSHORT}-rsg-leap-${ENV}-aks"
@@ -36,64 +32,37 @@ ACR_RG="${ORG}-${REGIONSHORT}-rsg-leap-${ENV}-acr"
 NW_RG="${ORG}-${REGIONSHORT}-rsg-leap-${ENV}-network"
 
 WORKSPACE_NAME="${ORG}-${REGIONSHORT}-law-leap-${ENV}-001"
-
-echo $HUB_RG
-echo $AKS_RG
-echo $ACR_RG
-echo $NW_RG
-echo $WORKSPACE_NAME
-
-#Log analytics
 AKS_WORKSPACE_RESOURCE_ID="/subscriptions/$SUBSCRIPTIONID/resourcegroups/$GEN_RG/providers/Microsoft.OperationalInsights/workspaces/$WORKSPACE_NAME"
-echo $AKS_WORKSPACE_RESOURCE_ID
 
-#AAD
 AAD_SERVER_APP_ID="d99f8e30-6873-4219-8981-94c2473d32b8"
 AAD_SERVER_APP_SECRET="pCS/QMoM9QgN9uFJ08GmbQ?[WGawOEm?"
 AAD_CLIENT_APP_ID="6d121ab4-7a85-4288-a7b6-e1cc8fa8b555"
 AAD_TENANT_ID="eb5e156e-540c-42da-8f8c-0cd5639f036a"
 
-# SQL
 SQL_NAME="${ORG}-${REGIONSHORT}-sqlmi-${ENV}-01"
 SQL_USERNAME="clydeandcosbadmin"
 SQL_PASSWORD="Zxcvbnm12345#####"
-echo $SQL_NAME
-echo $SQL_USERNAME
-echo $SQL_PASSWORD
 
-#VNET
 HUB_VNET_NAME="${ORG}-${REGIONSHORT}-vnet-hub-01"
 HUB_VNET_ADDRESS_PREFIX="10.125.240.0/20"
-echo $HUB_VNET_NAME
 
 SPOKE_VNET_NAME="${ORG}-${REGIONSHORT}-vnet-leap-${ENV}-01"
 SPOKE_VNET_ADDRESS_PREFIX="10.1.0.0/16"
-echo $SPOKE_VNET_NAME
 
-#SUBNET
 GW_SUBNET_NAME="${ORG}-${REGIONSHORT}-snet-gw"
 GW_SUBNET_ADDRESS_PREFIX="10.0.0.0/24"
 GW_NSG_NAME="${ORG}-${REGIONSHORT}-nsg-gw"
-echo $GW_SUBNET_NAME
-echo $GW_NSG_NAME
 
 SQL_SUBNET_NAME="${ORG}-${REGIONSHORT}-subnet-sql-${ENV}"
 SQL_SUBNET_ADDRESS_PREFIX="10.1.0.0/24"
 SQL_NSG_NAME="${ORG}-${REGIONSHORT}-nsg-sql"
-echo $SQL_SUBNET_NAME
-echo $SQL_NSG_NAME
 
 AKS_SUBNET_NAME="${ORG}-${REGIONSHORT}-leap-${ENV}-snet-aks-01"
 AKS_SUBNET_ADDRESS_PREFIX="10.125.0.0/22"
 AKS_NSG_NAME="${ORG}-${REGIONSHORT}-nsg-${ENV}-snet-aks-01"
-echo $AKS_SUBNET_NAME
-echo $AKS_NSG_NAME
 
-#ACR
 ACR_NAME="ccukssbacr"
-echo $ACR_NAME
 
-#AKS
 AKS_CLUSTER_NAME="${ORG}-${REGIONSHORT}-aks-leap-${ENV}-01"
 AKS_NODE_COUNT="3"
 AKS_MAX_PODS="30"
@@ -101,16 +70,36 @@ AKS_SERVICE_CIDR="10.125.4.0/24"
 AKS_DNS_SERVICE_IP="10.125.4.254"
 AKS_DOCKER_BRIDGE_ADDRESS="10.125.5.1/28"
 AKS_VERSION="1.13.12"
+
+echo $ENV
+echo $EnvFull
+echo $TAGS
+echo $HUB_RG
+echo $GEN_RG
+echo $AKS_RG
+echo $ACR_RG
+echo $NW_RG
+echo $WORKSPACE_NAME
+echo $AKS_WORKSPACE_RESOURCE_ID
+echo $HUB_VNET_NAME
+echo $SPOKE_VNET_NAME
+echo $GW_SUBNET_NAME
+echo $GW_NSG_NAME
+echo $SQL_SUBNET_NAME
+echo $SQL_NSG_NAME
+echo $AKS_SUBNET_NAME
+echo $AKS_NSG_NAME
+echo $SQL_NAME
+echo $SQL_USERNAME
+echo $SQL_PASSWORD
 echo $AKS_RG
 echo $AKS_CLUSTER_NAME
 echo $AKS_VERSION
+echo $ACR_NAME
 
+# DO STUFF FROM HERE
 
 # ----------- Create Resource Groups --------------
-#az group delete  --name $HUB_RG
-#az group delete  --name $SPOKE_RG
-#az group delete  --name $LOG_ANALYTICS_RG
-
 az group create --location $LOCATION --name $HUB_RG --tags $TAGS
 az group lock create --lock-type CanNotDelete -n lock -g $HUB_RG
 
